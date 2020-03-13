abedbbb0c836969b1f045a8ca1db21c2bcc7549f
GG Guitart <gertaguitart2019@u.northwestern.edu>
2020-03-13 13:32:14 -0500

    got the display to work horizontally for the cards

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,31 +1,44 @@
 import React, { Component } from "react";
 // import PropTypes from 'prop-types'
-import {connect} from 'react-redux';
-import {getPosts} from '../actions/postActions';
-import Card from 'react-bootstrap/Card'
-
+import { connect } from 'react-redux';
+import { getPosts } from '../actions/postActions';
+import Card from 'react-bootstrap/Card';
+import Flexbox from 'flexbox-react';
+import "../App.css";
+import { Grid } from '@material-ui/core';
 class Posts extends Component {
     componentDidMount() {
         this.props.getPosts()
     }
     render() {
-        const postItems = this.props.posts.map ( post => (
-            <div><Card>
-                <p> {post.paperName} </p>
-                <p> {post.paperAuthor} </p>
-                <p> {post.videoCreator} </p></Card>
-            </div> 
-        )) 
+       
+        const postItems = this.props.posts.map(post => (
+                // <div className="cards">
+                    <Grid item xs>
+                    <Card style={{ width: '18rem' }}>
+                        <p>Paper Title: {post.paperName} </p>
+                        <p>Paper Author: {post.paperAuthor} </p>
+                        <p>Video Creator: {post.videoCreator} </p>
+                    </Card>
+                  </Grid>
+                // </div>
+        ))
+       
         return (
-        
-            <div>
-                <h1> Posts </h1>
-                {postItems} 
-            </div>
-           
+
+
+<div>
+    <Grid 
+    container spacing = {5}
+
+    >
+                    {postItems} 
+                    </Grid>
+</div>
         )
 
-}}
+    }
+}
 // Posts.propTypes = {
 //     getPosts: PropTypes.func.isRequired,
 //     posts: PropTypes.array.isRequired
@@ -34,5 +47,5 @@ class Posts extends Component {
 const mapStateToProps = state => ({
     posts: state.posts.items
 })
-  
-export default connect(mapStateToProps , {getPosts}) (Posts);
+
+export default connect(mapStateToProps, { getPosts })(Posts);
